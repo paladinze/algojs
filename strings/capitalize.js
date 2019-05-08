b2d4b51b5077f75aca7e09e43330826e497f8170
paladinze <paladinze@hotmail.com>
2019-05-08 13:02:14 +0800

    capitalize every word

---
+++ strings/capitalize.js
@@ -0,0 +1,18 @@
+
+// capitalize the first letter of each word in the string then
+
+function capitalize(str) {
+  let result = str[0].toUpperCase();
+
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+}
+
+module.exports = capitalize;
